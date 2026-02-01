cf3cf5e90baee9388a2f2bc2690f51aab4788f53
enyasystem <enyaelvis@gmail.com>
2026-02-01 19:53:45 +0100

    Add service worker registration to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js").then((registration) => {
+        console.log("Service Worker registered:", registration);
+      }).catch((error) => {
+        console.log("Service Worker registration failed:", error);
+      });
+    }
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
